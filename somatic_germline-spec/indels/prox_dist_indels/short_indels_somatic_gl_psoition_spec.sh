73aca9ab5fd7f31b8527ea736258ae500404e0fa
Simon Timmins <s2559269@node2a02.ecdf.ed.ac.uk>
2025-08-27 09:46:00 +0100

    pts files for prox/dist germline/somatic specififc PTs

---
+++ somatic_germline-spec/indels/prox_dist_indels/short_indels_somatic_gl_psoition_spec.sh
@@ -0,0 +1,40 @@
+#housekeeping FP PT
+#!/bin/bash
+
+# Grid Engine options go here, e.g.:
+#$ -cwd
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=64G
+#$ -l rl9=true
+#$ -m ae
+#$ -t 1-5
+
+
+source /etc/profile.d/modules.sh
+module load R/4.3
+fp_list=(zero
+/exports/igmm/eddie/semple-lab/stimmins/chipseeker/chipseeker_nbl_footprints/specificity/germline_specific_distal.bed
+/exports/igmm/eddie/semple-lab/stimmins/chipseeker/chipseeker_nbl_footprints/specificity/germline_specific_promoter_prox.bed
+/exports/igmm/eddie/semple-lab/stimmins/chipseeker/chipseeker_nbl_footprints/specificity/somatic_specific_distal.bed
+/exports/igmm/eddie/semple-lab/stimmins/chipseeker/chipseeker_nbl_footprints/specificity/somatic_specific_promoter_prox.bed
+)
+
+peaks_names=(zero
+germline_specific_distal
+germline_specific_promoter_prox
+somatic_specific_distal
+somatic_specific_promoter_prox)
+atac=${fp_list[${SGE_TASK_ID}]};
+peak_name=${peaks_names[${SGE_TASK_ID}]};
+echo $peaks
+echo $peak_name
+vcf=/exports/igmm/eddie/semple-lab/stimmins/gnomad_v4/1811/gnomAD_v4_short_ins1811.vcf
+name=${peak_name}_fps_short_ins
+atac=/exports/igmm/eddie/semple-lab/stimmins/peak_specificity/nbl/germline_specific_fps.ms.bed
+output=$name'.pdf'
+echo $vcf
+echo $name
+echo $atac
+echo $output
+echo 'now running rscript'
+Rscript /exports/igmm/eddie/semple-lab/stimmins/perms/xia_oocyte/indels/generic_pt_masked.R $vcf $name $atac $output
